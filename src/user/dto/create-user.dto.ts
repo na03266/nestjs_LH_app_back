3476d6a804a155f3cd5cb7032085e5ee5ec845a3
나황제 <na03266@livewalk.co.kr>
2025-09-10 14:54:11 +0900

    (no commit message provided)

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+export class CreateUserDto {
+  // 이름
+  mbName: string;
+  // 비밀번호
+  mbPassword: string;
+  // 사번
+  mbId: string;
+  // 상위부서
+  // 부서
+  mb1: string;
+  // 직위
+  mb2: string;
+  // 직무
+  mb5: string;
+  // 연락처
+  mbTel: string;
+  // 연락처(폰)
+  mbHp: string;
+  // 이메일
+  mbEmail: string;
+
+}
