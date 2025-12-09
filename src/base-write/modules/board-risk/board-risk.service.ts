cad486c0d0ebc08b8455b247c307893a2c16316d
나황제 <na03266@livewalk.co.kr>
2025-12-09 14:09:45 +0900

    feat. 수정 메소드 권한 수정

--- src/base-write/modules/board-risk/board-risk.service.ts
+++ src/base-write/modules/board-risk/board-risk.service.ts
@@ -10,6 +10,7 @@ import {G5Board} from "../../../board/entities/g5-board.entity";
 import {FileService} from "../../../common/file/file.service";
 import {CommonService} from "../../../common/common.service";
 import {ConfigService} from "@nestjs/config";
+import {GetPostsDto} from "../../dto/get-posts.dto";
 
 
 @Injectable()
@@ -35,4 +36,15 @@ export class BoardRiskService extends AbstractWriteService<BoardRisk> {
 
     // 필요하면 여기서 개별 게시판만의 커스텀 메서드/오버라이드 추가
     // 예: findAll에 기본 caName 필터 강제 등
+    override async findAll(dto: GetPostsDto) {
+        const nextDto: GetPostsDto = {
+            ...dto,
+            // 예: caName이 안 들어왔으면 '위험성평가'를 기본값으로
+            caName: dto.caName ?? '위험성평가',
+            // wr1, wr2 등도 여기에서 강제 가능
+        };
+
+        // 부모의 findAll 쿼리 구조는 그대로 사용
+        return super.findAll(nextDto);
+    }
 }
